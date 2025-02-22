952c14e21860a8c95acfb245a5506d61119f53b0
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-22 23:44:33 +0200

    Initial commit

---
+++ js/script-1.js
@@ -0,0 +1,8 @@
+const mamo = 'Перемінна "mamo" в script-1';
+
+console.log(mamo);
+
+const bamo = 'Перемінна "bamo" в script-1';
+
+console.log(bamo);
+
---
+++ js/script-2.js
@@ -0,0 +1,4 @@
+const mamo = 'Перемінна "mamo" в script-2';
+console.log(mamo);
+const vamo = 'Перемінна "vamo" в script-2';
+console.log(vamo);
